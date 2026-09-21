e0832926219851e2cb3ecdc0159d32603afe1ff2
Masterproof <basic.alutrix@gmail.com>
2026-09-21 09:33:17 -0700

    Add files via upload

---
+++ google-config.js
@@ -0,0 +1,12 @@
+// Exodus Inc Google configuration
+// IMPORTANT: Because GitHub Pages is public/static, this file is also public.
+// Restrict your API key to:
+//   https://masterproof.github.io/*
+//   https://docs.google.com/*
+// and only the Google Drive API + Google Picker API.
+
+window.EXODUS_GOOGLE_CONFIG = {
+  clientId: 868821411419-6amgp0hnu4bvb55gqlbs0upn3rhl3f6c.apps.googleusercontent.com,
+  apiKey: AIzaSyCj7Iz_Dz6N6l7JLLdhAfEeZiuNnt1-jg0,
+  appId: 868821411419
+};
